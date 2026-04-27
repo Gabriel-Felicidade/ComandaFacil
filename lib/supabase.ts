32ac15431a6aed9687f7bfb36053995174ed6728
Filipe <filipeangelo.fa@gmail.com>
2026-04-27 12:07:26 -0300

    Ajuste no supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// O createBrowserClient garante que o login seja salvo nos Cookies para o Middleware ler
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
